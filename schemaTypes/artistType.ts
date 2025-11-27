bd78427c04b684f1843b5dfdc187664b567bd426
Volodymyr Klymenko <volodymyr.klymenko@vaimo.com>
2025-11-27 13:02:46 +0100

    Type generated

--- schemaTypes/artistType.ts
+++ schemaTypes/artistType.ts
@@ -16,6 +16,7 @@ export const artistType = defineType({
                                              defineField({
                                                              name: 'photo',
                                                              type: 'image',
+                                                             options: { hotspot: true },
                                                          }),
                                          ],
                                      })
